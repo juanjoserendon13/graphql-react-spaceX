ecf5b03828171c7d0155f95ba0a13b29e488219f
Juan José Rendón <juan.jose@netmidas.com>
2019-07-05 18:22:11 -0500

    graphql with schema configured

--- schema.js
+++ schema.js
@@ -1,4 +1,13 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
+} = require("graphql");
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
 // Launch Type
 const LaunchType = new GraphQLObjectType({
@@ -8,6 +17,70 @@ const LaunchType = new GraphQLObjectType({
     mission_name: { type: GraphQLString },
     launch_year: { type: GraphQLString },
     launch_date_local: { type: GraphQLString },
-    launch_sucess: { type: GraphQLBoolean }
+    launch_sucess: { type: GraphQLBoolean },
+    rocket: { type: RocketType }
   })
+});
+
+// Rocket Type
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      async resolve(parent, args) {
+        const response = await fetch("https://api.spacexdata.com/v3/launches");
+        const data = await response.json();
+        return data;
+      }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      async resolve(parent, args) {
+        const response = await fetch(
+          `https://api.spacexdata.com/v3/launches/${args.flight_number}`
+        );
+        const data = await response.json();
+        return data;
+      }
+    },
+    rockets: {
+      type: new GraphQLList(LaunchType),
+      async resolve(parent, args) {
+        const response = await fetch("https://api.spacexdata.com/v3/rockets");
+        const data = await response.json();
+        return data;
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        rocket_id: { type: GraphQLString }
+      },
+      async resolve(parent, args) {
+        const response = await fetch(
+          `https://api.spacexdata.com/v3/rockets/${args.rocket_id}`
+        );
+        const data = await response.json();
+        return data;
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
