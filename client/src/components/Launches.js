28e7767ae0cfe6a02984a99e3967c58856613e5e
Juan José Rendón <juan.jose@netmidas.com>
2019-07-10 10:24:28 -0500

    launch component added

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -27,7 +27,6 @@ class Launches extends Component {
             ({ loading, error, data }) => {
               if (loading) return <h4>Loading...</h4>;
               if (error) console.log(error);
-              console.log(data);
               return (
                 <Fragment>
                   {data.launches.map(l => (
